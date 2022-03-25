f22aa01f8168aaa52db26f9edc8b8374ac0a1f52
ljenu <ljenuin@gmail.com>
2022-03-24 21:19:18 -0400

    added login.js
    Co-authored-by: moonmaster36 <moonmaster36@users.noreply.github.com>
    Co-authored-by: tylersnel <tylersnel@users.noreply.github.com>
    Co-authored-by: alexwilso <alexwilso@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
+import Login from './Components/Login';
 import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="outer-container">
       <Navbar/>
+      <Login/>
 
 
     </div>
