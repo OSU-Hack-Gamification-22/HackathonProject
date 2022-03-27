d0c1b89afd0ce194424c09281b894690c5178fd0
ljenu <ljenuin@gmail.com>
2022-03-27 04:52:17 -0400

    updated nav link hardcoded data for visual.

--- src/Components/pages/About.js
+++ src/Components/pages/About.js
@@ -2,13 +2,7 @@ import React from 'react'
 
 function About() {
   return (
-    <>
     <h1>About</h1>
-    <br/>
-    <p>Foodie is a place where people around the world come together for one purpose. The enjoyment of food.
-      Users from all over the US can share their love for food and show that they are the top indulgers.
-    </p>
-    </>
   )
 }
 
